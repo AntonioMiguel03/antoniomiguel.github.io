c5c3cdf1b3ba26ee8c914f3cd5d97da7b92d7e8f
Antônio Miguel <88382914+AntonioMiguel03@users.noreply.github.com>
2024-10-07 07:20:54 -0300

    Update programs.js

--- javascript/programs.js
+++ javascript/programs.js
@@ -1,69 +1,120 @@
+//folha do javascript
+
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
 
-function show() {
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
-
-function close() {
+function close(){
     mainMenu.style.top = '-100%';
 }
 
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({ pageLanguage: "pt" }, 'google_translate_element');
+/*debugger;*/
+function googleTranslateElementInit() {	
+  new google.translate.TranslateElement({ pageLanguage: "pt" }, 'google_translate_element');
 }
 
-var flagImage = document.getElementById("flagImage");
+var flagImage = document.getElementById("flagImage");  
 flagImage.title = "Translate to English";
 
 function changeLanguageByButtonClick() {
-    var languageInput = document.getElementById("language").value;
-    var flagImage = document.getElementById("flagImage");
+ /*debugger; F10 vai para frente, F8 vai para o final do codigo*/
+  var languageInput = document.getElementById("language").value;
+  var selectField = document.querySelector("#google_translate_element select");
+  var flagImage = document.getElementById("flagImage");  
+  
+  flagImage.title = "Translate to English";
 
+  if (flagImage) {
     if (flagImage.src.includes("uk_flag.jpg")) {
-        // Mudar para o idioma português
-        flagImage.src = "img/flag_of_Brazil.png";
-        languageInput = "pt"; // Muda para português
-
-        // Muda o idioma para português usando a API do Google Translate
-        setLanguage(languageInput);
-        flagImage.title = "Traduzir para o Inglês";
+      // Alterar para bandeira do Brasil e idioma para "en"
+      flagImage.src = "img/flag_of_Brazil.png";
+      languageInput = "en";	  
+	  
+	  flagImage.title = "Traduzir para o Português";
     } else {
-        // Mudar para o idioma inglês
-        flagImage.src = "img/uk_flag.jpg";
-        languageInput = "en"; // Muda para inglês
-
-        // Muda o idioma para inglês usando a API do Google Translate
-        setLanguage(languageInput);
-        flagImage.title = "Traduzir para o Português";
+      // Alterar para bandeira do Reino Unido e idioma para "pt"
+      flagImage.src = "img/uk_flag.jpg";
+      languageInput = "pt";
+	  /*location.reload();*/
+	  
+	  changeLanguagePortuguese();
+	  return;
     }
 
-    // Atualiza o valor do input oculto
-    document.getElementById("language").value = languageInput;
+    for (var i = 0; i < selectField.children.length; i++) {
+      var option = selectField.children[i];
+      // Encontrar o idioma desejado e alterar o idioma anterior do campo de seleção oculto
+      if (option.value == languageInput) {
+        selectField.selectedIndex = i;
+		
+        // Acionar o evento 'change' posteriormente para traduzir usando a biblioteca do Google
+        selectField.dispatchEvent(new Event('change'));		
+        break;		
+      }
+    }
+  }
 }
 
-// Função para definir o idioma na API do Google Translate
-function setLanguage(language) {
-    var googleTranslateElement = new google.translate.TranslateElement({ pageLanguage: 'pt' }, 'google_translate_element');
-    var selectField = document.createElement("select");
-    selectField.style.display = "none"; // Oculta o select
-    selectField.value = language; // Define o valor do idioma
+function changeLanguagePortuguese() {	
+	var languageInput = "pt";
+	var selectField = document.querySelector("#google_translate_element select");
 
-    var option = document.createElement("option");
-    option.value = language;
-    selectField.appendChild(option);
-  
-    // Adiciona o select ao DOM (para o Google Translate funcionar)
-    document.getElementById("google_translate_element").appendChild(selectField);
-    
-    // Dispara o evento de mudança
-    selectField.dispatchEvent(new Event('change'));
-    
-    // Remove o select após a mudança para não deixar no DOM
-    selectField.remove();
+	for (var i = 0; i < selectField.children.length; i++) {
+	  var option = selectField.children[i];
+	  // Encontrar o idioma desejado e alterar o idioma anterior do campo de seleção oculto
+	  if (option.value == languageInput) {
+		selectField.selectedIndex = i;
+		
+		// Acionar o evento 'change' posteriormente para traduzir usando a biblioteca do Google
+		selectField.dispatchEvent(new Event('change'));		
+		location.reload();
+		break;		
+	  }
+	}
+}
+
+
+
+
+
+
+
+//Botao de voltar ao Menu
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  // Mova o usuário para o topo da página
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+
+  // Remova o fragmento da URL
+  if (history.replaceState) {
+    // Use history.replaceState para modificar a URL sem adicionar uma nova entrada ao histórico
+    history.replaceState(null, document.title, window.location.pathname + window.location.search);
+  } else {
+    // Como fallback, modifique a propriedade hash (isso adicionará uma nova entrada ao histórico)
+    window.location.hash = '';
+  }
 }
